f9d28203b2729abe35945f0136c864ee404a1c22
Udd Robert <robert.udd@lm.se>
2020-12-18 15:42:49 +0100

    Probably in the wrong direction

--- day18/part1.py
+++ day18/part1.py
@@ -1,17 +1,24 @@
 def calc(exp):
     if len(exp) == 1:
         return int(exp[0])
+
     val1 = exp.pop()
     oper = exp.pop()
     val2 = exp.pop()
-    if oper == '+':
-        return calc(exp + [int(val1) + int(val2)])
+    if val1[0] == '(':
+        calc(exp + [val2, oper, val1[1:]])
+    elif val2[-1] == ')' and oper == '+':
+        return int(val1) + int(val2)
+    elif val2[-1] == ')' and oper == '*':
+        return int(val1) * int(val2)
+    elif oper == '+':
+        return calc(exp + [str(int(val1) + int(val2))])
     elif oper == '*':
-        return calc(exp + [int(val1) * int(val2)])
+        return calc(exp + [str(int(val1) * int(val2))])
     else:
         print('ERROR')
 
-with open('test1') as f:
+with open('test2') as f:
     for l in f:
         exp = l.split()
         exp.reverse()
